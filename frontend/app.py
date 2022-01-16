17d10362419c96d9e36b27dbea6213e18b084ca7
Adam Kowalczyk <kombajno678@gmail.com>
2022-01-16 22:42:32 +0100

    basic working frontend

--- frontend/app.py
+++ frontend/app.py
@@ -1,35 +1,94 @@
-from flask import Flask, make_response, redirect, render_template, request, send_file
-from flask_bootstrap import Bootstrap
+
+from flask import Flask, Response, make_response, redirect, render_template, request, send_file, url_for
 import os
 import json
 import numpy as np
-
 import matplotlib.pyplot as plt
-
+import matplotlib.dates as mdates
+from datetime import datetime
+import memcached as mc
+import pandas as pd
 
 PORT = int(os.environ.get('PORT', 8080))
 
 app = Flask(__name__)
-Bootstrap(app)
+
+pcs = ['pc1', 'pc2', 'pc3']
 
 
-@app.route('/graph', methods=['GET'])
-def get_store():
-    graphFilename = 'graph.png'
+def handleJsonResponse(data):
+    return Response(json.dumps(data),  mimetype='application/json')
 
 
-    # Data for plotting
-    t = np.arange(0.0, 2.0, 0.01)
-    s = 1 + np.sin(2 * np.pi * t)
+@app.route('/pc', methods=['GET'])
+def get_pcs():
+    cache = mc.Cache()
+    return handleJsonResponse(cache.get_pcs())
 
+
+@app.route('/pc/<pcname>', methods=['GET'])
+def get_pc_by_name(pcname):
+    cache = mc.Cache()
+    return handleJsonResponse(cache.get_pc_keys(pcname))
+
+
+@app.route('/pc/<pcname>/<int:lasthours>', methods=['GET'])
+def get_pc_dataEntries(pcname, lasthours):
+    cache = mc.Cache()
+    return handleJsonResponse(cache.get_pc_data(pcname, lasthours))
+
+
+@app.route('/pc/<pcname>/<int:lasthours>/graph/<resourcetype>', methods=['GET'])
+def get_pc_graph(pcname, lasthours, resourcetype='cpu'):
+    if resourcetype is None:
+        resourcetype = 'cpu'
+    cache = mc.Cache()
+    data = cache.get_pc_data(pcname, lasthours)
+    graphFilename = f'img/{pcname}_{resourcetype}_-{lasthours}h.png'
+
+    dataList = list(data.items())
+    systemInfoEntries = [x[1] for x in dataList]
+
+    timestamps = [datetime.strptime(x['time'][0:19].replace(
+        "T", " "), '%Y-%m-%d %H:%M:%S') for x in systemInfoEntries]
+    dates = [datetime.strptime(f"{x['time'][0:10]}T00:00:00".replace(
+        "T", " "), '%Y-%m-%d %H:%M:%S') for x in systemInfoEntries]
+    dates_h = [datetime.strptime(f"{x['time'][0:13]}:00:00".replace(
+        "T", " "), '%Y-%m-%d %H:%M:%S') for x in systemInfoEntries]
+    # times = [x['time'][10:15] for x in systemInfoEntries]
+
+    df = pd.DataFrame(
+        {
+            'timestamp': timestamps,
+            'date':dates,
+            'date_h':dates_h,
+            # 'time':times,
+            'cpu': [x['cpu']['total'] for x in systemInfoEntries],
+            'mem': [(x['memory']['used'] / x['memory']['total'])
+                    * 100 for x in systemInfoEntries]
+        })
+    
     fig, ax = plt.subplots()
-    ax.plot(t, s)
+    fig.autofmt_xdate()
+    ax.set_ylim(0, 100)
+    ax.set(xlabel='time', ylabel=f'{resourcetype} usage (%)',
+           title=f'Total {resourcetype} usage of {pcname}')
+    
+    if len(df) > 100:
+        df = df.groupby('date_h').mean().reset_index()
+        ax.plot(df['date_h'], df[resourcetype])
+    else:
+        ax.plot(df['timestamp'], df[resourcetype])
+    
 
-    ax.set(xlabel='time (s)', ylabel='voltage (mV)',
-        title='About as simple as it gets, folks')
     ax.grid()
 
-    
+    if lasthours < 24:
+        xfmt = mdates.DateFormatter('%H:%M')
+    else:
+        xfmt = mdates.DateFormatter('%m-%d %H:%M')
+
+    ax.xaxis.set_major_formatter(xfmt)
 
     plt.savefig(graphFilename)
     return send_file(graphFilename, mimetype='image/png')
@@ -37,8 +96,21 @@ def get_store():
 
 @app.route('/', methods=['GET'])
 def get_index():
-    return render_template('index.html')
+    cache = mc.Cache()
+    pc = request.args.get("pc")
+    view = request.args.get("view", 1, type=int)
+    resource = request.args.get("resource", "cpu")
+    all_pcs = cache.get_pcs()
+
+    return render_template('index.html', all_pcs=all_pcs, pc=pc, view=view, resource=resource)
 
 
 if __name__ == '__main__':
+    cache = mc.Cache()
+    cache.generate_sample_data()
+
     app.run(port=PORT)
+    app.add_url_rule('/favicon.ico',
+                     redirect_to=url_for('static', filename='favicon.ico'))
+    app.add_url_rule('/icon.png',
+                     redirect_to=url_for('static', filename='icon.png'))
---
+++ frontend/memcached.py
@@ -0,0 +1,129 @@
+from pymemcache import serde
+from pymemcache.client import base
+import json
+import time
+import datetime
+import random
+import base64
+import bz2
+from datetime import timedelta
+MC_ADDRESS = 'localhost'
+MC_PORT = 11211
+PCS_KEY = 'pcs'
+
+
+class JsonSerde(object):
+    def serialize(self, key, value):
+        if isinstance(value, str):
+            return JsonSerde.compress(JsonSerde.endoceBase64(value)), 1
+        return JsonSerde.compress(JsonSerde.endoceBase64(json.dumps(value))), 2
+
+    def deserialize(self, key, value, flags):
+        value = JsonSerde.decodeBase64(JsonSerde.decompress(value))
+        if flags == 1:
+            return value
+        if flags == 2:
+            return json.loads(value)
+        raise Exception("Unknown serialization format")
+
+    def endoceBase64(msg):
+        encodedBytes = base64.b64encode(msg.encode("utf-8"))
+        return str(encodedBytes, "utf-8")
+
+    def decodeBase64(msg):
+        decodedBytes = base64.b64decode(msg)
+        return str(decodedBytes, "utf-8")
+
+    def compress(msg):
+        return bz2.compress(msg.encode('utf-8'))
+
+    def decompress(data):
+        return bz2.decompress(data).decode('utf-8')
+
+
+class Cache:
+    def __init__(self):
+        self._client = Cache.getMemcachedClient()
+
+    def getMemcachedClient():
+        print(
+            f"connecting to memcached at: {MC_ADDRESS}:{MC_PORT} ... ", end="")
+        client = base.Client((MC_ADDRESS, MC_PORT),  serde=serde.pickle_serde)
+        # client.cache_memlimit(1024)
+        # print(client.stats())
+        print("success")
+        return client
+
+    def generate_sample_data(self):
+        def generate_pc_info(pcname):
+            currentDatetimeIso = datetime.datetime.utcnow().isoformat()
+            data = {
+                "pcname": pcname,
+                "time": currentDatetimeIso,
+                "system": {
+                    "system": "Windows",
+                    "name": "ADAMKO-DM",
+                    "version": "10.0.22000",
+                    "machine": "AMD64",
+                    "boot": "2022/1/16 13:41:38"
+                },
+                "cpu": {
+                    "cores": 4,
+                    "logical_cores": 8,
+                    "usage": {
+                        "0": random.randrange(15, 35, 1),
+                        "1": random.randrange(15, 35, 1),
+                        "2": random.randrange(15, 35, 1),
+                        "3": random.randrange(15, 35, 1),
+                        "4": random.randrange(15, 35, 1),
+                        "5": random.randrange(15, 35, 1),
+                        "6": random.randrange(15, 35, 1),
+                        "7": random.randrange(15, 35, 1)
+                    },
+                    "total": random.randrange(20, 40, 1)
+                },
+                "memory": {
+                    "total": 32*1024,
+                    "available": 16*1024,
+                    "used": random.randrange(3, 5)*1024
+                }
+            }
+            return data
+        self._client.flush_all()
+        sample_pcs = [f'pc-{i}' for i in range(3)]
+        self._client.set(PCS_KEY, sample_pcs)
+        # generate sample data
+        for pc in sample_pcs:
+            pc_data_keys = []
+            for i in range(11_000):
+                date = datetime.datetime.utcnow() - timedelta(minutes=i)
+                key = f"{pc}/{date.isoformat()}"
+                pc_data_keys.append(key)
+                data = generate_pc_info(pc)
+                data['time'] = date.isoformat()
+                self._client.set(key, data)
+            self._client.set(pc, pc_data_keys)
+
+    def get_pcs(self):
+        return self._client.get(PCS_KEY)
+
+    def get_pc_keys(self, pcname):
+        return self._client.get(pcname)
+
+    def get_pc_data(self, pcname, fromLastHours=1):
+        keys = self._client.get(pcname)
+        n = datetime.datetime.utcnow()
+        d = timedelta(hours=fromLastHours, minutes=0)
+        fromDatetime = n - d
+
+
+        keys = list(filter(lambda x:
+                           datetime.datetime.fromisoformat(x.split('/')[1]) > fromDatetime, keys))
+
+        chunk_size = 200
+        chunked_keys = [keys[i:i+chunk_size]
+                        for i in range(0, len(keys), chunk_size)]
+        result = {}
+        for chunk in chunked_keys:
+            result = result | self._client.get_many(chunk)
+        return result
